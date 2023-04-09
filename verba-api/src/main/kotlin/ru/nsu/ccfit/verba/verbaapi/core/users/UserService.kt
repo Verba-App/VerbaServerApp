062941288731c859c3d56552553496df81cb5b63
Rodion Kotov <r.kotov@g.nsu.ru>
2023-04-09 18:08:15 +0700

    verba-api/feature #14 Bug: - Ошибка при исполнении verba-db-init.sql в тестах

--- verba-api/src/main/kotlin/ru/nsu/ccfit/verba/verbaapi/core/users/UserService.kt
+++ verba-api/src/main/kotlin/ru/nsu/ccfit/verba/verbaapi/core/users/UserService.kt
@@ -1,3 +1,8 @@
 package ru.nsu.ccfit.verba.verbaapi.core.users
 
 import org.springframework.stereotype.Service
+
+@Service
+class UserService{
+
+}
---
+++ verba-api/src/test/kotlin/ru/nsu/ccfit/verba/verbaapi/TestContainerDbContextInitializer.kt
@@ -0,0 +1,42 @@
+package ru.nsu.ccfit.verba.verbaapi
+
+import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.testcontainers.containers.PostgreSQLContainer
+
+
+val pgContainer: PostgreSQLContainer<*> by lazy {
+    PostgreSQLContainer("postgres:15")
+        .withExposedPorts(15435)
+        .withUsername("test-verba-postgres")
+        .withPassword("postgres")
+        .withDatabaseName("postgres")
+        .withTmpFs(mapOf("/var" to "rw"))
+        .withEnv("PGDATA", "/var/lib/postgresql/data-no-mounted")
+        .withCommand("-c max_connections=400")
+        .withReuse(true)
+        .withDatabaseName("hello")
+        //.withInitScript("db/verba-db-init.sql")
+        .apply {
+            start()
+        }
+}
+
+class TestContainerDbContextInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    override fun initialize(applicationContext: ConfigurableApplicationContext) {
+        applicationContext.overrideProperties(
+            "spring.datasource.url" to pgContainer.jdbcUrl,
+            "spring.datasource.username" to "postgres",
+            "spring.datasource.password" to "postgres"
+        )
+    }
+}
+
+fun ConfigurableApplicationContext.overrideProperties(vararg properties: Pair<String, String>) {
+    TestPropertyValues
+        .of(properties.toMap())
+        .applyTo(this)
+}
+
