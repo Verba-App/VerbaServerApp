526d53ddfd5fdb56b64bce65296ec2fb50fcf482
d.martyshkin <d.martyshkin@g.nsu.ru>
2023-04-14 21:49:01 +0700

    verba-data/feature #7 Рефакторинг:  - Заголовки теперь вносятся через перехватчик Bugfix:  - параметры запроса вносятся через @Value

--- verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/api/openai/inter/OpenAiApiInterface.java
+++ verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/api/openai/inter/OpenAiApiInterface.java
@@ -2,7 +2,6 @@ package ru.nsu.ccfit.verba.verbadata.api.openai.inter;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 import ru.nsu.ccfit.verba.verbadata.api.openai.dto.GeneratorRequestDto;
@@ -10,7 +9,5 @@ import ru.nsu.ccfit.verba.verbadata.api.openai.dto.GeneratorRequestDto;
 public interface OpenAiApiInterface {
 
     @POST("v1/images/generations")
-    Call<String> generate(@Header("Content-Type") String contentType,
-                                    @Header("Authorization") String apiKey,
-                                    @Body GeneratorRequestDto request);
+    Call<String> generate(@Body GeneratorRequestDto request);
 }
--- verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/api/openai/service/ImageService.java
+++ verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/api/openai/service/ImageService.java
@@ -1,6 +1,5 @@
 package ru.nsu.ccfit.verba.verbadata.api.openai.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -11,19 +10,14 @@ import ru.nsu.ccfit.verba.verbadata.api.openai.inter.OpenAiApiInterface;
 import java.io.IOException;
 
 @Service
-@RequiredArgsConstructor
 public class ImageService {
-
-
-    private static final String contentType= "application/json";
-    private static final String apiKey = "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";
 
     @Autowired
     private OpenAiApiInterface service;
 
 
     public String generateImage(GeneratorRequestDto request) throws IOException {
-        Call<String> retrofitCall = service.generate(contentType,  "Bearer " + apiKey, request);
+        Call<String> retrofitCall = service.generate(request);
 
         Response<String> response = retrofitCall.execute();
 
