69f4d706c3e17e8b5ca67d916ca9e92a6304a60e
d.martyshkin <d.martyshkin@g.nsu.ru>
2023-04-28 23:55:11 +0700

    verba-data/feature #7 Bug:  - не исполняется метод generateImage

---
+++ verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/api/openai/enumeration/Code.java
@@ -0,0 +1,21 @@
+package ru.nsu.ccfit.verba.verbadata.api.openai.enumeration;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum Code {
+
+    OK(HttpStatus.OK,"УСПЕШНЫЙ ЗАПРОС"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "НЕВЕРНЫЙ ЗАПРОС"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "ОТСУТСТВУЮТ ПРАВА ДОСТУПА"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ВНУТРЕНЯЯ ОШИБКА СЕРВЕРА"),
+    SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "СЕРВИС НЕДОСТУПЕН"),
+    GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "ВРЕМЯ ОЖИДАНИЯ ИСТЕКЛО");
+
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+}
