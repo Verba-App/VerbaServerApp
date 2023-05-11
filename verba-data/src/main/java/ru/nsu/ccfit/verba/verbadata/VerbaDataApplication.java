47b9a54d399afc18c54bb1b172158555a118f6b2
k.babaev <k.babaev@g.nsu.ru>
2023-05-11 16:02:08 +0700

    verba-data/feature #24

--- verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/VerbaDataApplication.java
+++ verba-data/src/main/java/ru/nsu/ccfit/verba/verbadata/VerbaDataApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class VerbaDataApplication {
 
 
+
     public static void main(String[] args) {
         SpringApplication.run(VerbaDataApplication.class, args);
     }
